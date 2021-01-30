9a320eb5932f33327c7e3cb328e24808ce9badf5
leberhard5869 <58434277+leberhard5869@users.noreply.github.com>
2021-01-30 15:41:12 -0500

    SDfasdf

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello</Text>;
+const HomeScreen = (props) => {
+  
+
+  return {
+    <View>
+      <Text style={styles.text}>Hello</Text>
+      <Button
+      title="Go to Components Demo"
+      onPress={() => props.navigation.navigate("Components")}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => props.navigation.navigate("List")}
+      />
+View> 
 };
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   text: {
@@ -12,4 +31,3 @@ const styles = StyleSheet.create({
 });
 
 export default HomeScreen;
-
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,15 +3,9 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
   const friends = [
-    { name: 'Friend #1'},
-    { name: 'Friend #2'},
-    { name: 'Friend #3'},
-    { name: 'Friend #4'},
-    { name: 'Friend #5'},
-    { name: 'Friend #6'},
-    { name: 'Friend #7'},
-    { name: 'Friend #8'},
-    { name: 'Friend #9'},
+    { name: 'Friend #1', age: 20},
+    { name: 'Friend #2', age: 34},
+    { name: 'Friend #3', age: 56},
  ];
 
   return (
@@ -19,7 +13,7 @@ const ListScreen = () => {
       keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={( { item }) => {
-        return <Text style={styles.textStyle}>{item.name}</Text>
+        return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>
       }}
     />
   );
@@ -27,7 +21,7 @@ const ListScreen = () => {
 
 const styles = StyleSheet.create({
   textStyle: {
-    marginVertical: 50
+    marginVertical: 5
   }
 });
 
